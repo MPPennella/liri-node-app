f774eba84ab67f388aebe214916257f42754aa4b
M. Pennella <michael.pennella@gmail.com>
2018-10-17 00:54:51 -0700

    Added basic switching logic and required packages

--- liri.js
+++ liri.js
@@ -1,2 +1,30 @@
+
+require("node-spotify-api")
+require("request")
+require("moment")
 require("dotenv").config()
 let keys = require("./keys")
+
+let cmd = process.argv[2]
+
+switch (cmd) {
+    case "concert-this":
+        ;
+        break;
+    case "spotify-this-song":
+        ;
+        break;
+    case "movie-this":
+        ;
+        break;
+    case "do-what-it-says":
+        ;
+        break;
+    default:
+        console.log(`Invalid command '${cmd}', valid commands are as follows:`);
+        console.log("* concert-this [artist name]");
+        console.log("* spotify-this-song [song name]");
+        console.log("* movie-this [movie name]");
+        console.log("* do-what-it-says");
+        break;
+}
